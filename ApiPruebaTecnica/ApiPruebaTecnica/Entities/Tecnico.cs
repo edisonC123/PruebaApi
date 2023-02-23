50e4144bf6fb34ea382620b1c73cc6c48b8aed33
edison Cordoba <ecordobac@celsia.com>
2023-02-23 14:24:44 -0500

    Se crea Proyecto api

---
+++ ApiPruebaTecnica/ApiPruebaTecnica/Entities/Tecnico.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiPruebaTecnica.Entities
+{
+    public class Tecnico
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public Double  SueldoBase { get; set; }
+    }
+}
